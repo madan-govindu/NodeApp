7b0cdc8c21764835046a767cf7547ee19b7eaeac
madan-govindu <95535957+madan-govindu@users.noreply.github.com>
2021-12-19 10:29:18 +0000

    Added Madan PC IP and Nodes js text

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello NodeApp!! \n");
+  response.end("Hello Madan!! \n");
 });
 
 // listen on localhost:8000
 server.listen(8000);
-console.log("Server listening on port 8000 :  http://127.0.0.1:8000/");
+console.log("Server listening on port 8000 :  http://192.168.0.120:8000/");
